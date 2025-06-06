b505d64a35b11f0ed355895adb3861941169e7e1
Valerio <valerio.malvone@gmail.com>
2025-06-06 19:30:59 +0200

    modifica utils

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -5,5 +5,5 @@ public class Utils {
     public static final String[] COLONNE_LISTA_PARTECIPANTI = {"","ID", "Nome", "Cognome"};
     public static final String[] COLONNE_LISTA_INVITI = {"ID", "Nome Team", "Email partecipante"};
     public static final String[] COLONNE_LISTA_TEAM = {"ID", "Nome Team"};
-    public static final String[] COLONNE_LISTA_HACKATHON = {"ID", "Nome Hackathon", "Descrizione", " "};
+    public static final String[] COLONNE_LISTA_HACKATHON = {"ID", "Nome Hackathon", "Descrizione"};
 }
