5f46d091e20f062d5df4b4483810810cb2be53ce
Assandre17 <alessio.somma17@gmail.com>
2025-06-24 19:39:11 +0200

    add constants for tipo users

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -6,4 +6,8 @@ public class Utils {
     public static final String[] COLONNE_LISTA_INVITI = {"ID", "Nome Team", "Email partecipante", "Nome Hackathon"};
     public static final String[] COLONNE_LISTA_TEAM = {"ID", "Nome Team"};
     public static final String[] COLONNE_LISTA_HACKATHON = {"ID", "Nome Hackathon", "Descrizione"};
+
+    public static final String TIPO_GIUDICE = "GIUDICE";
+    public static final String TIPO_ORGANIZZATORE = "ORGANIZZATORE";
+    public static final String TIPO_PARTECIPANTE = "PARTECIPANTE";
 }
